48d04791c89e5189f846ba31e26622a63febf3e3
Jesper <jesperminks7@gmail.com>
2021-12-01 10:45:43 +0100

    Start scherm aangepast

--- AUSfp/Start.Designer.cs
+++ AUSfp/Start.Designer.cs
@@ -30,38 +30,179 @@ namespace AUSfp
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Start));
-            this.button1 = new System.Windows.Forms.Button();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SearchBar = new System.Windows.Forms.TextBox();
+            this.SearchIcon = new System.Windows.Forms.PictureBox();
+            this.RocLogo = new System.Windows.Forms.PictureBox();
+            this.LoginBtn = new System.Windows.Forms.PictureBox();
+            this.LogoutBtn = new System.Windows.Forms.PictureBox();
+            this.ManageStockBtn = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RocLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoutBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ManageStockBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // NameLabel
             // 
-            this.button1.Location = new System.Drawing.Point(544, 77);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 49);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.NameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(931, 61);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(381, 33);
+            this.NameLabel.TabIndex = 1;
+            this.NameLabel.Text = "Welkom, Jesper Minks";
+            this.NameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.panel1.Location = new System.Drawing.Point(29, 149);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1650, 3);
+            this.panel1.TabIndex = 2;
+            // 
+            // SearchBar
+            // 
+            this.SearchBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.SearchBar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SearchBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.SearchBar.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.SearchBar.Location = new System.Drawing.Point(297, 48);
+            this.SearchBar.Margin = new System.Windows.Forms.Padding(4);
+            this.SearchBar.Multiline = true;
+            this.SearchBar.Name = "SearchBar";
+            this.SearchBar.Size = new System.Drawing.Size(611, 68);
+            this.SearchBar.TabIndex = 6;
+            this.SearchBar.TabStop = false;
+            // 
+            // SearchIcon
+            // 
+            this.SearchIcon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.SearchIcon.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SearchIcon.BackgroundImage")));
+            this.SearchIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SearchIcon.Location = new System.Drawing.Point(860, 61);
+            this.SearchIcon.Margin = new System.Windows.Forms.Padding(4);
+            this.SearchIcon.Name = "SearchIcon";
+            this.SearchIcon.Size = new System.Drawing.Size(48, 46);
+            this.SearchIcon.TabIndex = 34;
+            this.SearchIcon.TabStop = false;
+            this.SearchIcon.Click += new System.EventHandler(this.SearchIcon_Click);
+            // 
+            // RocLogo
+            // 
+            this.RocLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.RocLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("RocLogo.BackgroundImage")));
+            this.RocLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.RocLogo.Location = new System.Drawing.Point(29, 48);
+            this.RocLogo.Margin = new System.Windows.Forms.Padding(4);
+            this.RocLogo.Name = "RocLogo";
+            this.RocLogo.Size = new System.Drawing.Size(203, 68);
+            this.RocLogo.TabIndex = 36;
+            this.RocLogo.TabStop = false;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.LoginBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LoginBtn.BackgroundImage")));
+            this.LoginBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.LoginBtn.Location = new System.Drawing.Point(1444, 39);
+            this.LoginBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(89, 77);
+            this.LoginBtn.TabIndex = 37;
+            this.LoginBtn.TabStop = false;
+            // 
+            // LogoutBtn
+            // 
+            this.LogoutBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogoutBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.LogoutBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LogoutBtn.BackgroundImage")));
+            this.LogoutBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.LogoutBtn.Location = new System.Drawing.Point(1544, 39);
+            this.LogoutBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.LogoutBtn.Name = "LogoutBtn";
+            this.LogoutBtn.Size = new System.Drawing.Size(89, 77);
+            this.LogoutBtn.TabIndex = 38;
+            this.LogoutBtn.TabStop = false;
+            // 
+            // ManageStockBtn
+            // 
+            this.ManageStockBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ManageStockBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ManageStockBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ManageStockBtn.BackgroundImage")));
+            this.ManageStockBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ManageStockBtn.Location = new System.Drawing.Point(1322, 39);
+            this.ManageStockBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.ManageStockBtn.Name = "ManageStockBtn";
+            this.ManageStockBtn.Size = new System.Drawing.Size(89, 77);
+            this.ManageStockBtn.TabIndex = 39;
+            this.ManageStockBtn.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 192);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 72;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(515, 748);
+            this.dataGridView1.TabIndex = 40;
             // 
             // Start
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(1650, 964);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1714, 970);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ManageStockBtn);
+            this.Controls.Add(this.LogoutBtn);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.RocLogo);
+            this.Controls.Add(this.SearchIcon);
+            this.Controls.Add(this.SearchBar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.NameLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Start";
             this.Text = "AUSfp";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Start_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SearchIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RocLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoutBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ManageStockBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox SearchBar;
+        private System.Windows.Forms.PictureBox SearchIcon;
+        private System.Windows.Forms.PictureBox RocLogo;
+        private System.Windows.Forms.PictureBox LoginBtn;
+        private System.Windows.Forms.PictureBox LogoutBtn;
+        private System.Windows.Forms.PictureBox ManageStockBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- AUSfp/Start.cs
+++ AUSfp/Start.cs
@@ -27,5 +27,10 @@ namespace AUSfp
         {
 
         }
+
+        private void SearchIcon_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
