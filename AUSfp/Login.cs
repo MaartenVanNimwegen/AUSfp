6b2c66ee0cb35e9ce1efcbf1d654afbc0aa1bbf7
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2021-12-01 10:44:43 +0100

    werkend login systeem

--- AUSfp/Login.cs
+++ AUSfp/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace AUSfp
 {
@@ -17,9 +18,48 @@ namespace AUSfp
             InitializeComponent();
         }
 
-        private void Login_Load(object sender, EventArgs e)
+        /// <summary>
+        /// username inputfield wordt leeg gemaakt wanneer er op word geklikt.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void usernameInput_Click(object sender, EventArgs e)
         {
-
+            usernameInput.Text = "";
+        }
+        /// <summary>
+        /// passwordfield wordt leeg gemaakt wanneer er op word geklikt.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void passwordInput_Click(object sender, EventArgs e)
+        {
+            passwordInput.Text = "";
+        }
+        /// <summary>
+        /// Wanneer de loginBtn word geklikt word er ingelogd.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = new MySqlConnection("Data Source = localhost; Initial Catalog = testdatabase; User ID = root; Password = ");
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand("select * from users where username = '" + usernameInput.Text + "' AND password = '" + passwordInput.Text + "'", con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("Gebruikersnaam en wachtwoord komen niet overeen!", "Er is een fout opgetreden", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            usernameInput.Text = string.Empty;
+            passwordInput.Text = string.Empty;
+            reader.Close();
+            cmd.Dispose();
+            con.Close();        
         }
     }
 }
